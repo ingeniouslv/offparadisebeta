3bb73b0c206849f222693374726737881392c89f
ingeniouslv <herb@ingeniouslv.com>
2014-09-30 12:15:49 -0700

    updated single page title

--- content-single.php
+++ content-single.php
@@ -5,14 +5,12 @@
 	        			<div class="post-title hidden-phone banner">
 	        				<div class="social-icons"></div>
 	        				
-	        			<?php if( get_post_format() ): ?>	
 	        				<div class="title-meta">
 	        					<h2 class="title-1">OFF⋅PARADISE</h2>
 	        					<?php the_title( '<h3 class="title-2">', '</h3>' ); ?>
 	        					<time class="date"><?php _s_posted_on(); ?></time>
 	        					<figure class="author"></figure>
 	        				</div>
-	        			<?php endif; ?>
 	        			
 	        			</div>
 	        			
